a46208615674c4af94c585b0e04372fcd85f01bb
PangHae <yds05074@gmail.com>
2022-02-12 18:09:55 +0900

    [pangehae] add LoginPage

---
+++ Front/sugarfree/src/pages/LoginPage/Loginpage.jsx
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+function LoginField(){
+    const [userInfo, setUserInfo] = useState({
+        userId : "",
+        userPwd: ""
+    });
+
+    return (
+        <div id='login-div'>
+            <fieldset>
+            <legend>Input your Id and Password</legend>
+            <InputField data={userInfo} onChange={setUserInfo}/>
+            </fieldset>
+        </div>
+    );
+}
+
+function InputField({data, onChange}){
+
+    const history = useHistory();
+
+    const handleChange = (e) =>{
+        const {name, value} = e.target;
+        
+        onChange({
+            ...data,
+            [name] : value
+        });
+    };
+
+    const handleClick = (e) =>{
+        e.preventDefault();
+        if(data.userId === "" || data.userPwd ===""){
+            alert("Fill out every form");
+        }
+        else{
+            console.log("Sending data to server...")
+            //Login API 정해지면 URL 변경하겠음
+            axios.post("http://127.0.0.1:5000/login/valid/", {
+                    userId : data.userId,
+                    userPwd : data.userPwd
+            })
+            .then((res) =>{
+                const result = res.data.success;
+                console.log("response : " + result);
+                if(result === "valid"){
+                    history.push("/mainPage");
+                }else{
+                    if(result ==="id invalid"){
+                        alert("Wrong Id. Please Check Again");
+                    }else{
+                        alert("Wrong Pwd. Please Check Again");
+                    }
+                }
+            })
+            .catch(error =>{
+                console.log(error)
+            });
+            onChange({
+                userId:"",
+                userPwd:""
+            });
+        }
+    };
+
+    return (
+        <form>
+            <b>ID</b>
+            <br/>
+            <input
+                type="text"
+                placeholder='ID' 
+                name="userId" 
+                onChange={handleChange}
+                value={data.userId}/>
+            <br/>
+            <b>PASSWORD</b>
+            <input
+                type="password"
+                placeholder='PWD' 
+                name="userPwd" 
+                onChange={handleChange}
+                value={data.userPwd}/>
+            <br/>
+            <button onClick={handleClick}>Submit</button>
+        </form>
+    );
+}
+
+export default LoginField;
