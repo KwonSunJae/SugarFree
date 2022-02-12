0f5b4a3a29d08d9decd35ec390c8dff3f83edf87
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-02-12 17:13:53 +0900

    Routes Update
    sunjae did

--- Front/sugarfree/src/App.js
+++ Front/sugarfree/src/App.js
@@ -1,9 +1,27 @@
 
 
+import React from "react";
+import { Route,Link,Routes } from "react-router-dom";
+
+import About from './components/About';
+import Home from './components/Home';
+
 function App() {
   return (
     <div >
-      <h1>hello wordl!</h1>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+    
+      <Routes>
+      <Route path="/" element={<Home></Home>} />
+      <Route path="/about" element={<About></About>} />
+      </Routes>
     </div>
   );
 }
