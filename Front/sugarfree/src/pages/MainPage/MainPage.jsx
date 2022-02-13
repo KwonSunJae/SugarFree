2e0d0b88a2b2626e86dce5ec43498035c67a0dd7
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-02-13 16:28:06 +0900

    modify

--- Front/sugarfree/src/pages/MainPage/MainPage.jsx
+++ Front/sugarfree/src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import candyMachine from 'Front/sugarfree/public/img/candyMachine_512.png';
+
 
 
 const MainPage = () => {
@@ -11,9 +11,9 @@ const MainPage = () => {
     <div style={{ backgroundColor: '#F2DBD8' }}>
       <h3>가입자 수 : {setUserNum}</h3>
       <h3>전달된 선물 수 : {setCandyNum}</h3>
-      <img src={candyMachine} alt="candyMachine" />
+      <img src={"/img/candyMachine_512.png"} alt="candyMachine" />
       <br></br>
-      <button type="button"><img src="Front/sugarfree/public/img/btn_login_light_250.png" height="80" width="100" /></button>
+      <button type="button"><img src="/img/btn_login_light_250.png" height="80" width="100" /></button>
     </div>
   );
 }
