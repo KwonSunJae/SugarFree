90f41c188fbb1b037746020a40a66cbc68818610
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-06-29 03:59:13 +0900

    Update: server no use port

--- Front/src/utils/api.js
+++ Front/src/utils/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: "http://dev-api-tutor.openinfra-kr.org:5051",
+    baseURL: "http://dev-api-tutor.openinfra-kr.org",
     headers:{
         "Content-Type" : "application/json",
 
--- Server/app/server.js
+++ Server/app/server.js
@@ -2,11 +2,21 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-
+const cors = require("cors");
 dotenv.config();
+const whitelist = ["http://dev-front-tutor.openinfra-kr.org/"];
 
+const corsOptions ={
+  origin: function(origin, callback){
+    if(whitelist.indexOf(origin)!==-1){
+      callback(null,true);
+    }else{
+      callback(new Error("Not allowed Origin!"));
+    }
+  }
+}
 const api = require("./src/routes/index");
-
+app.use(cors());
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
