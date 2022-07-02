dd9f0d0293d678b25bf25dbc6f2c3eef27172286
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-07-03 06:28:43 +0900

    Update setup.sh

--- bin/Front/setup.sh
+++ bin/Front/setup.sh
@@ -21,23 +21,20 @@ git fetch
 git pull
 # nginx config 파일 배치
 cd /etc/nginx/sites-enabled
-rm -rf default
+sudo rm -rf default
 
-cp /home/ubuntu/SugarFree/.config/front/default .
+sudo cp /home/ubuntu/SugarFree/.config/front/default .
 
 #certbot을 통해 ssl 적용
 sudo apt-get install -y snapd
-sudo snap install -y core; sudo snap refresh core
-sudo apt-get remove certbot
-sudo snap install --classic -y certbot
+sudo snap install  core; sudo snap refresh core
+sudo apt-get remove -y certbot
+sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 
 sudo certbot --nginx
-ksun4131@gmail.com
-Y
-Y
-dev-front-tutor.openinfra-kr.org
+
 cd /home/ubuntu/SugarFree/Front
 yarn install
 yarn build
