17cb929dc2f4b2d0b5251febdd3e9df0a35423d1
rehoosa <janghoosa@gmail.com>
2022-08-09 20:29:03 +0900

    Modify: remove db.connect

--- Server/app/src/config/db.js
+++ Server/app/src/config/db.js
@@ -8,44 +8,44 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT,
 });
 
-db.connect((err) => {
-  if (err) return console.error("error" + err.message);
+// db.connect((err) => {
+//   if (err) return console.error("error" + err.message);
 
-  let createTableUsers = `CREATE TABLE IF NOT EXISTS users(
-    user_id VARCHAR(20) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    nickname VARCHAR(10) NOT NULL,
-    question_num INT NOT NULL,
-    question_answer VARCHAR(30) NOT NULL,
-    PRIMARY KEY(user_id)
-  )`;
+//   let createTableUsers = `CREATE TABLE IF NOT EXISTS users(
+//     user_id VARCHAR(20) NOT NULL,
+//     password VARCHAR(100) NOT NULL,
+//     nickname VARCHAR(10) NOT NULL,
+//     question_num INT NOT NULL,
+//     question_answer VARCHAR(30) NOT NULL,
+//     PRIMARY KEY(user_id)
+//   )`;
 
-  let createTableCandy = `CREATE TABLE IF NOT EXISTS candy(
-    candy_num INT NOT NULL AUTO_INCREMENT,
-    message VARCHAR(2000),
-    emoji_num VARCHAR(50) NOT NULL,
-    img MEDIUMBLOB,
-    sender_name VARCHAR(20) NOT NULL,
-    user_id VARCHAR(20) NOT NULL,
-    send_time TIMESTAMP NOT NULL DEFAULT NOW(),
-    PRIMARY KEY(candy_num)
-  )`;
+//   let createTableCandy = `CREATE TABLE IF NOT EXISTS candy(
+//     candy_num INT NOT NULL AUTO_INCREMENT,
+//     message VARCHAR(2000),
+//     emoji_num VARCHAR(50) NOT NULL,
+//     img MEDIUMBLOB,
+//     sender_name VARCHAR(20) NOT NULL,
+//     user_id VARCHAR(20) NOT NULL,
+//     send_time TIMESTAMP NOT NULL DEFAULT NOW(),
+//     PRIMARY KEY(candy_num)
+//   )`;
 
-  let createTableReport = `CREATE TABLE IF NOT EXISTS report(
-    candy_num INT NOT NULL,
-    content VARCHAR(100),
-    PRIMARY KEY(candy_num)
-  )`;
+//   let createTableReport = `CREATE TABLE IF NOT EXISTS report(
+//     candy_num INT NOT NULL,
+//     content VARCHAR(100),
+//     PRIMARY KEY(candy_num)
+//   )`;
 
-  db.query(createTableUsers, (err, result, field) => {
-    if (err) throw err;
-  });
-  db.query(createTableCandy, (err, result, field) => {
-    if (err) throw err;
-  });
-  db.query(createTableReport, (err, result, field) => {
-    if (err) throw err;
-  });
-});
+//   db.query(createTableUsers, (err, result, field) => {
+//     if (err) throw err;
+//   });
+//   db.query(createTableCandy, (err, result, field) => {
+//     if (err) throw err;
+//   });
+//   db.query(createTableReport, (err, result, field) => {
+//     if (err) throw err;
+//   });
+// });
 
 module.exports = db;
