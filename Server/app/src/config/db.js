3fd4b2096627c23444dc6af69eaeb28fea6ee8e1
rehoosa <janghoosa@gmail.com>
2022-08-13 14:50:22 +0900

    Modify: Change MySQL to get connection from pool

--- Server/app/src/config/db.js
+++ Server/app/src/config/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+// });
 
 // db.connect((err) => {
 //   if (err) return console.error("error" + err.message);
@@ -48,4 +48,39 @@ const db = mysql.createConnection({
 //   });
 // });
 
-module.exports = db;
+
+var pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+module.exports = {
+  query: function(){
+      var sql_args = [];
+      var args = [];
+      for(var i=0; i<arguments.length; i++){
+          args.push(arguments[i]);
+      }
+      var callback = args[args.length-1]; //last arg is callback
+      pool.getConnection(function(err, connection) {
+      if(err) {
+              console.log(err);
+              return callback(err);
+          }
+          if(args.length > 2){
+              sql_args = args[1];
+          }
+      connection.query(args[0], sql_args, function(err, results) {
+        connection.release(); // always put connection back in pool after last query
+        if(err){
+                  console.log(err);
+                  return callback(err);
+              }
+        callback(null, results);
+      });
+    });
+  }
+};
